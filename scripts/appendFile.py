d1c17d03102f80d2e05c05ad0b9d5be2aea85fda
Paul Kalauner <paul.kalauner@student.tuwien.ac.at>
2020-03-17 18:58:31 +0100

    Remove unnecessary indentation

--- scripts/appendFile.py
+++ scripts/appendFile.py
@@ -19,7 +19,7 @@ if len(sys.argv) != 4:
 
 try:
     with open(sys.argv[1], 'rb') as in1, open(sys.argv[2], 'rb') as in2, open(sys.argv[3], 'wb') as out:
-                out.write(in1.read())
-                out.write(in2.read())
+        out.write(in1.read())
+        out.write(in2.read())
 except FileNotFoundError:
     sys.exit("Error: One or more files could not be opened.")
