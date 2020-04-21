3bc44b913cc863f30d0abba668b197fc8a22cb54
Paul Kalauner <paul.kalauner@student.tuwien.ac.at>
2020-04-21 15:47:30 +0200

    Remove GCE blocks after ambiguous GIF generation

--- scripts/createAmbiguousGIF.py
+++ scripts/createAmbiguousGIF.py
@@ -18,8 +18,11 @@ import imageio
 if len(sys.argv) != 4:
     sys.exit("Usage: python3 createAmbiguousGIF.py <Image 1> <Image 2> <Output file>")
 
+# Graphic Control Extension Identifier
+GCE = binascii.unhexlify("21f9")
+
 try:
-    with open(sys.argv[1], 'rb') as img1, open(sys.argv[2], 'rb') as img2, open(sys.argv[3], 'wb') as out:
+    with open(sys.argv[1], 'rb') as img1, open(sys.argv[2], 'rb') as img2, open(sys.argv[3], 'w+b') as out:
         # Check filename extension of out file
         if out.name[out.name.rfind('.'):] != '.gif':
             print("Warning: Filename extension of output file should probably be '.gif'.")
@@ -34,8 +37,23 @@ try:
             images.append(image1)
         images.append(image2)
         
-        # Write all images to one GIF with very high FPS, so that the 'Delay Time' (see GIF spec) is undefined
-        imageio.mimwrite(out, images, format='GIF', fps=1000)
+        # Write all images to one GIF
+        imageio.mimwrite(out, images, format='GIF')
+
+        # Remove Graphic Control Extension blocks
+        out.seek(0)
+        content = out.read()
+        out.seek(0)
+        out.truncate()
+
+        gce_pos = content.find(GCE)
+        while gce_pos >= 0:
+            new_content = content[:gce_pos]
+            new_content += content[gce_pos+8:]
+            content = new_content
+            gce_pos = content.find(GCE)
+            
+        out.write(content)
 
 except FileNotFoundError:
     sys.exit("Error: One or more files could not be opened.")
