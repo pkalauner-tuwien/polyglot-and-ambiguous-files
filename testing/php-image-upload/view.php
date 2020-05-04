127097d719be77d4b76dee30a616175742bbfc86
Paul Kalauner <paul.kalauner@student.tuwien.ac.at>
2020-05-04 15:39:59 +0200

    Remove inline style from testpages

--- testing/php-image-upload/view.php
+++ testing/php-image-upload/view.php
@@ -7,6 +7,6 @@
     if (!file_exists($imagepath)) {
         echo "<h4>Image $imagepath does not exist.</h4>";
     } else {
-        echo "<img style='border: 2px solid black; max-width:60%;' src='$imagepath'>";
+        echo "<img src='$imagepath'>";
     }
 ?>
